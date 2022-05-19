450049dccf6c7fe20849eebb38c8dbff3d7cf0a4
Danica Erediano <danica@newlogic.com>
2022-05-19 15:34:29 +0800

    removed console

--- screens/Scan/ScanScreenController.ts
+++ screens/Scan/ScanScreenController.ts
@@ -59,7 +59,6 @@ export function useScanScreen({ navigation }: MainRouteProps) {
   }
 
   useEffect(() => {
-    console.log('test');
     const subscriptions = [
       navigation.addListener('focus', () =>
         scanService.send(ScanEvents.SCREEN_FOCUS())
