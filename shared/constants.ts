501087e2d75d5c719cf825fc42e4acb5749a767f
anil_majji <majjianilkumar050@gmail.com>
2022-09-16 16:21:15 +0530

    Fixed PR conflicts

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,13 +1,25 @@
 import { VC } from '../types/vc';
+import {
+  MIMOTO_HOST,
+  GOOGLE_NEARBY_MESSAGES_API_KEY,
+} from 'react-native-dotenv';
 
-export const HOST = 'https://api-internal.qa4.mosip.net/residentmobileapp';
+export const HOST = MIMOTO_HOST;
 
 export const MY_VCS_STORE_KEY = 'myVCs';
+
 export const RECEIVED_VCS_STORE_KEY = 'receivedVCs';
 
 export const VC_ITEM_STORE_KEY = (vc: Partial<VC>) =>
   `vc:${vc.idType}:${vc.id}:${vc.requestId}`;
 
 export const ACTIVITY_LOG_STORE_KEY = 'activityLog';
+
 export const SETTINGS_STORE_KEY = 'settings';
+
 export const ONBOARDING_STATUS_STORE_KEY = 'isOnboardingDone';
+
+export const GNM_API_KEY = GOOGLE_NEARBY_MESSAGES_API_KEY;
+
+// https://developers.google.com/android/reference/com/google/android/gms/nearby/messages/Message#MAX_CONTENT_SIZE_BYTES
+export const GNM_MESSAGE_LIMIT = 102400 - 6400; // allowance for metadata
