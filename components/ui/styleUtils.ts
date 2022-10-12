5fee766a456205da9a5b627693f1165459b216b1
srikanth716 <srikanthsri7447@gmail.com>
2022-10-12 18:39:51 +0530

    changed to purple theme

--- components/ui/styleUtils.ts
+++ components/ui/styleUtils.ts
@@ -3,7 +3,7 @@ import { DefaultTheme } from './themes/DefaultTheme';
 
 // To change the theme, CSS theme file has to import and assign it to Theme in line no 6
 
-export const Theme = DefaultTheme;
+export const Theme = PurpleTheme;
 
 type SpacingXY = [number, number];
 type SpacingFull = [number, number, number, number];
