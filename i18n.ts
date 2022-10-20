d3d3afed924f886f019a759f94bcd7422fce3fd6
Monobikash Das <monobikash.das@mindtree.com>
2022-10-20 19:07:21 +0530

    Adding multi lang

--- i18n.ts
+++ i18n.ts
@@ -4,13 +4,21 @@ import { initReactI18next } from 'react-i18next';
 
 import en from './locales/en.json';
 import fil from './locales/fil.json';
+import ara from './locales/ara.json';
+import hin from './locales/hin.json';
+import kan from './locales/kan.json';
+import tam from './locales/tam.json';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const resources = { en, fil };
+const resources = { en, fil, ara, hin, kan, tam };
 
 export const SUPPORTED_LANGUAGES = {
   en: 'English',
   fil: 'Filipino',
+  ara: 'Arabic',
+  hin: 'Hindi',
+  kan: 'Kannada',
+  tam: 'Tamil',
 };
 
 i18next
