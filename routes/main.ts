2555673f5f188d910850b63ab5a6526a220c30e4
Pooja Babusing <poojababusing@Poojas-Laptop.local>
2023-01-19 10:15:46 +0530

    feat(#305): [Pooja] [iOS] Hiding Qr generration till ios supports verifier

--- routes/main.ts
+++ routes/main.ts
@@ -9,43 +9,50 @@ import { RootStackParamList } from './index';
 import { RequestLayout } from '../screens/Request/RequestLayout';
 import { ScanLayout } from '../screens/Scan/ScanLayout';
 import i18n from '../i18n';
+import { Platform } from 'react-native';
+import { USE_BLE_SHARE } from 'react-native-dotenv';
 
-export const mainRoutes: TabScreen[] = [
-  {
-    name: 'Home',
-    component: HomeScreen,
-    icon: 'home',
-    options: {
-      title: i18n.t('MainLayout:home'),
-    },
+var home: TabScreen = {
+  name: 'Home',
+  component: HomeScreen,
+  icon: 'home',
+  options: {
+    title: i18n.t('MainLayout:home'),
   },
-  {
-    name: 'Scan',
-    component: ScanLayout,
-    icon: 'qr-code-scanner',
-    options: {
-      title: i18n.t('MainLayout:scan'),
-      headerShown: false,
-    },
+};
+var scan: TabScreen = {
+  name: 'Scan',
+  component: ScanLayout,
+  icon: 'qr-code-scanner',
+  options: {
+    title: i18n.t('MainLayout:scan'),
+    headerShown: false,
   },
-  {
-    name: 'Request',
-    component: RequestLayout,
-    icon: 'file-download',
-    options: {
-      title: i18n.t('MainLayout:request'),
-      headerShown: false,
-    },
+};
+var request: TabScreen = {
+  name: 'Request',
+  component: RequestLayout,
+  icon: 'file-download',
+  options: {
+    title: i18n.t('MainLayout:request'),
+    headerShown: false,
   },
-  {
-    name: 'Profile',
-    component: ProfileScreen,
-    icon: 'person',
-    options: {
-      title: i18n.t('MainLayout:profile'),
-    },
+};
+var profile: TabScreen = {
+  name: 'Profile',
+  component: ProfileScreen,
+  icon: 'person',
+  options: {
+    title: i18n.t('MainLayout:profile'),
   },
-];
+};
+
+export const mainRoutes: TabScreen[] = [];
+mainRoutes.push(home);
+mainRoutes.push(scan);
+!(Platform.OS === 'ios' && USE_BLE_SHARE === 'true') &&
+  mainRoutes.push(request);
+mainRoutes.push(profile);
 
 export type MainBottomTabParamList = {
   Home: {
