e98d332b1f4a4385a661d5a64aa7cad32deb3cd5
anil_majji <majjianilkumar050@gmail.com>
2023-02-01 11:30:36 +0530

    Replaces profileIconBg with IconContainer

--- components/EditableListItem.tsx
+++ components/EditableListItem.tsx
@@ -17,8 +17,9 @@ export const EditableListItem: React.FC<EditableListItemProps> = (props) => {
       style={{ display: props.display }}>
       <Icon
         name={props.Icon}
-        type={props.IconType}
-        size={22}
+        type="antdesign"
+        size={20}
+        style={Theme.Styles.IconContainer}
         color={Theme.Colors.Icon}
       />
       <ListItem.Content>
