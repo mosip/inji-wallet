f0bcc9d9f89ad4a12a00c674271c512bfe1e96fa
vijay <vijayskumarsuresh@gmail.com>
2023-03-08 13:15:48 +0530

    refactor(#611): [Vijay] update the default application theme

--- components/ui/styleUtils.ts
+++ components/ui/styleUtils.ts
@@ -3,7 +3,8 @@ import { PurpleTheme } from './themes/PurpleTheme';
 import { APPLICATION_THEME } from 'react-native-dotenv';
 
 // To change the theme, CSS theme file has to import and assign it to Theme in line no 6
-export const Theme = APPLICATION_THEME == 'orange' ? DefaultTheme : PurpleTheme;
+export const Theme =
+  APPLICATION_THEME.toLowerCase() === 'purple' ? PurpleTheme : DefaultTheme;
 
 type SpacingXY = [number, number];
 type SpacingFull = [number, number, number, number];
