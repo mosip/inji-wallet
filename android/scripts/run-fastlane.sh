e9966d928fafe4e3457e1746b36cc04dc5e384be
adityankannan-tw <adityan.kannan@thoughtworks.com>
2023-05-23 16:49:44 +0530

    Clean up the code base

---
+++ android/scripts/run-fastlane.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Move to correct folder
+cd ..
+
+#Install bundler using Gem
+yes | sudo gem install bundler
+
+#Install plugins for fastlane
+yes | sudo fastlane install_plugins
+
+#Run Fastlane 
+bundle exec fastlane android_build
