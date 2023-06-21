cb781958423954d7939a77eebc9a403046f852ae
anil_majji <majjianilkumar050@gmail.com>
2023-06-21 10:13:43 +0530

    Displaying Received cards option in settings (INJI-96)

--- screens/Home/ReceivedVcsTabController.ts
+++ screens/Home/ReceivedVcsTabController.ts
@@ -1,12 +1,12 @@
 import { useSelector } from '@xstate/react';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ActorRefFrom } from 'xstate';
 import { selectVcLabel } from '../../machines/settings';
 import {
   selectIsRefreshingReceivedVcs,
   selectReceivedVcs,
 } from '../../machines/vc';
-import {  vcItemMachine } from '../../machines/vcItem';
+import { vcItemMachine } from '../../machines/vcItem';
 import { GlobalContext } from '../../shared/GlobalContext';
 import { HomeScreenTabProps } from './HomeScreen';
 import {
@@ -17,16 +17,20 @@ import {
 export function useReceivedVcsTab(props: HomeScreenTabProps) {
   const service = props.service as ActorRefFrom<typeof ReceivedVcsTabMachine>;
   const { appService } = useContext(GlobalContext);
+  const [isVisible, setIsVisible] = useState(false);
   const settingsService = appService.children.get('settings');
   const vcService = appService.children.get('vc');
 
   return {
+    isVisible,
     vcLabel: useSelector(settingsService, selectVcLabel),
     vcKeys: useSelector(vcService, selectReceivedVcs),
 
     isRefreshingVcs: useSelector(vcService, selectIsRefreshingReceivedVcs),
 
-    VIEW_VC: (vcItemActor: ActorRefFrom<typeof  vcItemMachine>) =>
+    TOGGLE_RECEIVED_CARDS: () => setIsVisible(!isVisible),
+
+    VIEW_VC: (vcItemActor: ActorRefFrom<typeof vcItemMachine>) =>
       service.send(ReceivedVcsTabEvents.VIEW_VC(vcItemActor)),
     REFRESH: () => service.send(ReceivedVcsTabEvents.REFRESH()),
   };
