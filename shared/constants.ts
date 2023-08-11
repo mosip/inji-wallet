0b050ddce2daff08b70e4ed485009fc2c540ad13
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2023-08-11 13:58:11 +0530

    feat(INJI-267): add and export deviceId constant in constants file
    Use react-native-device-info library to get the unique identifier for each and every device

--- shared/constants.ts
+++ shared/constants.ts
@@ -5,6 +5,7 @@ import {
   GOOGLE_NEARBY_MESSAGES_API_KEY,
 } from 'react-native-dotenv';
 import { Argon2iConfig } from './commonUtil';
+import { getUniqueId } from 'react-native-device-info';
 
 export let HOST = MIMOTO_HOST;
 
@@ -83,3 +84,5 @@ export const argon2iConfigForUinVid: Argon2iConfig = {
 
 export const argon2iSalt =
   '1234567891011121314151617181920212223242526272829303132333435363';
+
+export const deviceId = getUniqueId();
