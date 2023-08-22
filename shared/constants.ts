914a2489b9ef5af5053795497b0541ef26752d0d
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2023-08-22 16:52:01 +0530

    feat(INJI-267): generate unique session id for every session of the app
    After launching the app until we kill the app we consider it as a single session

--- shared/constants.ts
+++ shared/constants.ts
@@ -7,6 +7,8 @@ import {
 } from 'react-native-dotenv';
 import { Argon2iConfig } from './commonUtil';
 import { getUniqueId } from 'react-native-device-info';
+import { generateSessionId } from './telemetry/TelemetryUtils';
+import { __SessionId } from './GlobalVariables';
 
 export let MIMOTO_BASE_URL = MIMOTO_HOST;
 export const ESIGNET_BASE_URL = ESIGNET_HOST;
@@ -84,5 +86,70 @@ export const argon2iConfigForUinVid: Argon2iConfig = {
 
 export const argon2iSalt =
   '1234567891011121314151617181920212223242526272829303132333435363';
+
+export const SESSION_ID_DICTIONARY = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
 export const deviceId = getUniqueId();
