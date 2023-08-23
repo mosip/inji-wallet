0574bb658ae90552015ff1d8683a095efa4883d1
Sri Kanth Kola <srikanthsri7447@gmail.com>
2023-08-23 14:47:05 +0530

    fix(INJI-262):pinned VC's audit logs are missing

--- shared/constants.ts
+++ shared/constants.ts
@@ -53,7 +53,3 @@ export const APP_ID_DICTIONARY = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L',
   'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
 ];
-
-export function isIOS(): boolean {
-  return Platform.OS === 'ios';
-}
