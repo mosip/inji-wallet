8a08f076195375f12e941e80369dac637271b57d
adityankannan-tw <adityan.kannan@thoughtworks.com>
2023-09-05 11:42:29 +0530

    Get build name from user and build the app

--- android/scripts/internal-build.sh
+++ android/scripts/internal-build.sh
@@ -7,4 +7,4 @@ yes | sudo gem install bundler
 
 yes | sudo fastlane install_plugins
 
-bundle exec fastlane android_build_internal
+bundle exec fastlane android_build_upload && android_build_internal
