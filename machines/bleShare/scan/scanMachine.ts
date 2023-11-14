fb0a941730577ae04aa4e3dea58526e3466b8914
Vijay <94220135+vijay151096@users.noreply.github.com>
2023-11-14 10:28:55 +0530

    fix(inji-522): enable the location service before initiating the vc sharing.
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- machines/bleShare/scan/scanMachine.ts
+++ machines/bleShare/scan/scanMachine.ts
@@ -22,13 +22,14 @@ import {subscribe} from '../../../shared/openIdBLE/walletEventHandler';
 import {
   check,
   checkMultiple,
-  PermissionStatus,
   PERMISSIONS,
+  PermissionStatus,
   requestMultiple,
   RESULTS,
 } from 'react-native-permissions';
 import {
   checkLocationPermissionStatus,
+  checkLocationService,
   requestLocationPermission,
 } from '../../../shared/location';
 import {CameraCapturedPicture} from 'expo-camera';
@@ -40,14 +41,14 @@ import {BLEError} from '../types';
 import Storage from '../../../shared/storage';
 import {VCMetadata} from '../../../shared/VCMetadata';
 import {
-  getStartEventData,
   getEndEventData,
-  sendStartEvent,
-  sendEndEvent,
-  sendImpressionEvent,
-  getImpressionEventData,
-  sendErrorEvent,
   getErrorEventData,
+  getImpressionEventData,
+  getStartEventData,
+  sendEndEvent,
+  sendErrorEvent,
+  sendImpressionEvent,
+  sendStartEvent,
 } from '../../../shared/telemetry/TelemetryUtils';
 import {TelemetryConstants} from '../../../shared/telemetry/TelemetryConstants';
 
@@ -679,8 +680,21 @@ export const scanMachine =
           },
         },
         checkingLocationService: {
-          initial: 'checkingPermissionStatus',
+          initial: 'checkLocationService',
           states: {
+            checkLocationService: {
+              invoke: {
+                src: 'checkLocationStatus',
+              },
+              on: {
+                LOCATION_ENABLED: {
+                  target: 'checkingPermissionStatus',
+                },
+                LOCATION_DISABLED: {
+                  target: 'disabled',
+                },
+              },
+            },
             checkingPermissionStatus: {
               invoke: {
                 src: 'checkLocationPermission',
@@ -714,6 +728,13 @@ export const scanMachine =
                 },
                 LOCATION_REQUEST: {
                   actions: 'openAppPermission',
+                },
+              },
+            },
+            disabled: {
+              on: {
+                LOCATION_REQUEST: {
+                  target: 'checkLocationService',
                 },
               },
             },
@@ -1092,6 +1113,12 @@ export const scanMachine =
 
         checkLocationPermission: () => callback => {
           checkLocationPermissionStatus(
+            () => callback(model.events.LOCATION_ENABLED()),
+            () => callback(model.events.LOCATION_DISABLED()),
+          );
+        },
+        checkLocationStatus: () => callback => {
+          return checkLocationService(
             () => callback(model.events.LOCATION_ENABLED()),
             () => callback(model.events.LOCATION_DISABLED()),
           );
--- shared/location.ts
+++ shared/location.ts
@@ -1,5 +1,5 @@
 import RNLocation from 'react-native-location';
-
+import LocationServicesDialogBox from 'react-native-android-location-services-dialog-box';
 // Initialize RNLocation
 RNLocation.configure({
   distanceFilter: 5.0, // Example configuration, adjust as needed
@@ -7,26 +7,26 @@ RNLocation.configure({
 
 export function checkLocationPermissionStatus(
   onEnabled: () => void,
-  onDisabled: () => void
+  onDisabled: () => void,
 ) {
   RNLocation.checkPermission({
     android: {
       detail: 'fine',
     },
   })
-    .then((granted) => {
+    .then(granted => {
       if (granted) {
         return onEnabled();
       } else {
         return onDisabled();
       }
     })
-    .catch((err) => console.log('Error getting location:', err));
+    .catch(err => console.log('Error getting location:', err));
 }
 
 export async function requestLocationPermission(
   onEnabled: () => void,
-  onDisabled: () => void
+  onDisabled: () => void,
 ) {
   try {
     const granted = await RNLocation.requestPermission({
@@ -41,5 +41,28 @@ export async function requestLocationPermission(
     }
   } catch (error) {
     console.log(error);
+  }
+}
+
+export async function checkLocationService(
+  onEnabled: () => void,
+  onDisabled: () => void,
+) {
+  try {
+    await LocationServicesDialogBox.checkLocationServicesIsEnabled({
+      message:
+        "<h2 style='color: #0af13e'>Use Location ?</h2>This app wants to change your device settings:<br/><br/>Use GPS, Wi-Fi, and cell network for location<br/><br/><a href='#'>Learn more</a>",
+      ok: 'YES',
+      cancel: 'NO',
+      enableHighAccuracy: true, // true => GPS AND NETWORK PROVIDER, false => GPS OR NETWORK PROVIDER
+      showDialog: true, // false => Opens the Location access page directly
+      openLocationServices: true, // false => Directly catch method is called if location services are turned off
+      preventOutSideTouch: false, //true => To prevent the location services popup from closing when it is clicked outside
+      preventBackClick: false, //true => To prevent the location services popup from closing when it is clicked back button
+      providerListener: true, // true ==> Trigger "locationProviderStatusChange" listener when the location state changes
+    });
+    onEnabled();
+  } catch (e) {
+    onDisabled();
   }
 }
