c3100353452d38a0a834fe53f76d1b8b0d8b4b96
anil_majji <majjianilkumar050@gmail.com>
2024-01-17 18:48:44 +0530

    [INJI-680]: Renamed as disconnect from navigateToHome as we are disconnecting the devices
    Signed-off-by: anil_majji <majjianilkumar050@gmail.com>

--- machines/bleShare/scan/scanMachine.ts
+++ machines/bleShare/scan/scanMachine.ts
@@ -161,7 +161,7 @@ export const scanMachine =
           target: '.checkStorage',
         },
         DISMISS: {
-          target: '#scan.reviewing.navigatingToHome',
+          target: '#scan.reviewing.disconnect',
         },
       },
       states: {
@@ -595,7 +595,7 @@ export const scanMachine =
               entry: ['logShared', 'sendVcShareSuccessEvent'],
               on: {
                 DISMISS: {
-                  target: 'navigatingToHome',
+                  target: 'disconnect',
                 },
               },
             },
@@ -606,7 +606,8 @@ export const scanMachine =
                 },
               },
             },
-            navigatingToHome: {
+            disconnect: {
+              //Renamed this to disconnect from navigateToHome as we are disconnecting the devices.
               invoke: {
                 src: 'disconnect',
               },
@@ -661,7 +662,7 @@ export const scanMachine =
               target: '#scan.reviewing.cancelling',
             },
             DISMISS: {
-              target: '#scan.reviewing.navigatingToHome',
+              target: '#scan.reviewing.disconnect',
             },
           },
         },
