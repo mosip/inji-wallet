5f02b4ad03a57e476eb5c8d3c562f78f9c610257
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-01-30 00:11:40 +0530

    [INJI-631] extract vcItemField name and value into two separate components for better usability
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- components/VC/common/VCItemField.tsx
+++ components/VC/common/VCItemField.tsx
@@ -6,7 +6,38 @@ import LinearGradient from 'react-native-linear-gradient';
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 import {Tooltip} from 'react-native-elements';
 
-export const VCItemField = ({
+export const VCItemFieldName = ({
+  verifiableCredential,
+  fieldName,
+  wellknown,
+}) => {
+  if (!verifiableCredential) {
+    return (
+      <ShimmerPlaceHolder
+        LinearGradient={LinearGradient}
+        width={150}
+        style={{marginBottom: 5, borderRadius: 5}}
+      />
+    );
+  }
+
+  return (
+    <>
+      {fieldName && (
+        <Text
+          testID={`${fieldName}Title`}
+          weight="regular"
+          size="smaller"
+          {...setTextColor(wellknown)}
+          style={[Theme.Styles.subtitle]}>
+          {fieldName}
+        </Text>
+      )}
+    </>
+  );
+};
+
+export const VCItemFieldValue = ({
   verifiableCredential,
   fieldName,
   fieldValue,
@@ -14,50 +45,42 @@ export const VCItemField = ({
 }) => {
   if (!verifiableCredential) {
     return (
-      <Column margin="9 0 0 0">
-        <ShimmerPlaceHolder
-          LinearGradient={LinearGradient}
-          width={150}
-          style={{marginBottom: 5, borderRadius: 5}}
-        />
-        <ShimmerPlaceHolder
-          LinearGradient={LinearGradient}
-          width={180}
-          style={{borderRadius: 5}}
-        />
-      </Column>
+      <ShimmerPlaceHolder
+        LinearGradient={LinearGradient}
+        width={180}
+        style={{borderRadius: 5}}
+      />
     );
   }
 
   return (
-    <Column margin="9 0 0 0">
+    <Tooltip
+      toggleOnPress={fieldValue.length > 20}
+      containerStyle={{
+        width: 200,
+        height: null,
+        overflow: 'hidden',
+      }}
+      skipAndroidStatusBar={true}
+      backgroundColor={Theme.Colors.Icon}
+      popover={<Text>{fieldValue}</Text>}>
       <Text
-        testID={`${fieldName}Title`}
-        weight="regular"
-        size="smaller"
-        {...setTextColor(wellknown)}
-        style={[Theme.Styles.subtitle]}>
-        {fieldName}
+        testID={`${fieldName}Value`}
+        weight="semibold"
+        numLines={1}
+        ellipsizeMode={'tail'}
+        style={[Theme.Styles.subtitle, setTextColor(wellknown)]}>
+        {fieldValue}
       </Text>
-      <Tooltip
-        toggleOnPress={fieldValue.length > 20}
-        containerStyle={{
-          width: 200,
-          height: null,
-          overflow: 'hidden',
-        }}
-        skipAndroidStatusBar={true}
-        backgroundColor={Theme.Colors.Icon}
-        popover={<Text>{fieldValue}</Text>}>
-        <Text
-          testID={`${fieldName}Value`}
-          weight="semibold"
-          numLines={1}
-          ellipsizeMode={'tail'}
-          style={[Theme.Styles.subtitle, setTextColor(wellknown)]}>
-          {fieldValue}
-        </Text>
-      </Tooltip>
+    </Tooltip>
+  );
+};
+
+export const VCItemField = props => {
+  return (
+    <Column margin="9 0 0 0">
+      <VCItemFieldName {...props} />
+      <VCItemFieldValue {...props} />
     </Column>
   );
 };
