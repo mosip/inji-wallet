ba11363f013b1fd6b05ecc0cb4aa9688b777d2a0
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-02-19 11:11:58 +0530

    [InjiMob-745] Implemented sharing error modal with respective CTA's and message when VC sharing has failed. (#1234)
    * [INJIMOB-745]: Implemented Error moda of sharing failure with title, message & CTAs were display based on Error_codes
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Implemented Error modal of sharing failure with title, message & CTAs were display based on Error_codes
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Updated respective text in all the locals
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Fixed the error of go to history button in success sharing status
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Resolved PR Merge conflicts
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: remove duplicate import which causes build issue
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIMOB-745]: Modified SharingStatusModal into dynamic props for buttons, text as per requirement
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Made changes in the rendering of respective button as per requirements
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Adjusted the alignment of bottom buttons & Made header as per wire frame
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Changed the verifier side  text messages of Error codes[TUV_UNK_001, TVV_UNK_001, 002, 003]
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Added accessible props to Pressable in SharingStatusModal
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * [INJIMOB-745]: Modified success screen icons in to SVG's
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    Co-authored-by: srikanth716 <srikanthsri7447@gmail.com>

--- components/ui/svg.tsx
+++ components/ui/svg.tsx
@@ -20,6 +20,9 @@ import OtpVerificationIcon from '../../assets/Otp_Verification_Icon.svg';
 import FlipCameraIcon from '../../assets/Flip_Camera_Icon.svg';
 import CameraCaptureIcon from '../../assets/Camera_Capture_Icon.svg';
 import SuccessLogo from '../../assets/Success_Message_Icon1.svg';
+import ErrorLogo from '../../assets/Error_Message_Icon.svg';
+import SuccessHomeIcon from '../../assets/Success_home_iocn.svg';
+import SuccessHistoryIcon from '../../assets/Success_history_iocn.svg';
 import NoInternetConnection from '../../assets/No_Internet_Connection.svg';
 import SomethingWentWrong from '../../assets/Something_Went_Wrong.svg';
 import MagnifierZoom from '../../assets/Magnifier_Zoom.svg';
@@ -257,6 +260,32 @@ export class SvgImage {
 
   static SuccessLogo() {
     return <SuccessLogo {...testIDProps('SuccessLogo')} />;
+  }
+
+  static SuccessHomeIcon() {
+    return (
+      <SuccessHomeIcon
+        {...testIDProps('SuccessHomeIcon')}
+        color1={Theme.Colors.linearGradientStart}
+        color2={Theme.Colors.linearGradientEnd}
+        stroke={Theme.Colors.IconBg}
+      />
+    );
+  }
+
+  static SuccessHistoryIcon() {
+    return (
+      <SuccessHistoryIcon
+        {...testIDProps('SuccessHistoryIcon')}
+        color1={Theme.Colors.linearGradientStart}
+        color2={Theme.Colors.linearGradientEnd}
+        stroke={Theme.Colors.IconBg}
+      />
+    );
+  }
+
+  static ErrorLogo() {
+    return <ErrorLogo {...testIDProps('ErrorLogo')} />;
   }
 
   static PermissionDenied() {
