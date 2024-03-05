4a1dc6912a0324f02fd1b6044078661b3feebcec
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-03-05 11:23:59 +0530

    [INJIMOB-855] revert qr consent file changes
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- screens/QrLogin/QrConsent.tsx
+++ screens/QrLogin/QrConsent.tsx
@@ -40,8 +40,7 @@ export const QrConsent: React.FC<QrConsentProps> = props => {
             style={Theme.TextStyles.small}
             weight="bold"
             margin={'10 0 0 0'}>
-            {getClientNameForCurrentLanguage(controller.clientName)}{' '}
-            {t('access')}
+            {getClientNameForCurrentLanguage(controller.clientName)} {t('access')}
           </Text>
         </Column>
         <ScrollView>
