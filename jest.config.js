be74bb71db2498d213389d993307d13afabc75ef
Harsh Vardhan <harsh59v@gmail.com>
2024-03-07 15:05:04 +0530

    [INJIMOB-314] kludge - skip checking types during unit tests
    * mock image imports globally
    * enable tests to run from any dir
    
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,11 @@ module.exports = {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.spec.json',
+        // TODO: Remove this to log the type mismatch errors later.
+        diagnostics: {
+          exclude: ['**'],
+        },
+        tsconfig: '<rootDir>/tsconfig.spec.json',
       },
     ],
   },
@@ -24,7 +28,8 @@ module.exports = {
   // Not quite sure about the reason.
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   transformIgnorePatterns: [
-    '/node_modules/(?!(@react-native|react-native|react-native-argon2|@react-navigation|react-native-elements|react-native-size-matters|react-native-ratings|expo-constants)/).*/',
+    '/node_modules/(?!(@react-native|react-native|react-native-argon2|@react-navigation|react-native-elements|react-native-size-matters|react-native-ratings|expo-constants|base58-universal|@react-native-*)/).*/',
+    'node_modules/(?!(react-native|@react-native|react-native-biometrics-changed|base58-universal)/)',
   ],
   setupFiles: [
     '<rootDir>/__mocks__/react-native.mock.js',
@@ -47,6 +52,9 @@ module.exports = {
   ],
   coverageDirectory: 'coverage',
   moduleNameMapper: {
+    // https://stackoverflow.com/a/54513338
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
     '^\\.\\/locales\\/en\\.json$': '<rootDir>/__mocks__/en.mock.json',
     '^@digitalbazaar/rsa-verification-key-2018$':
       '<rootDir>/__mocks__/rsa-verification-key-2018.js',
