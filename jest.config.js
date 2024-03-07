bae0efe5fcc104996ebd2f165cedb2252c31aea7
Harsh Vardhan <harsh59v@gmail.com>
2024-03-07 12:15:40 +0530

    [INJIMOB-314] fix tsconfig for project type
    Signed-off-by: Harsh Vardhan <harsh59v@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,21 @@ module.exports = {
   ...tsjPreset,
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.spec.json',
-      babelConfig: true,
-    },
-  },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '\\.snap$'],
   cacheDirectory: '.jest/cache',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    __DEV__: true,
+  },
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-    '^.+\\.tsx?$': ['ts-jest'],
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
   },
   // This line should be kept even with nothing to be ignored, otherwise the transform will not take place.
   // Not quite sure about the reason.
@@ -25,4 +26,31 @@ module.exports = {
   transformIgnorePatterns: [
     '/node_modules/(?!(@react-native|react-native|react-native-argon2|@react-navigation|react-native-elements|react-native-size-matters|react-native-ratings|expo-constants)/).*/',
   ],
+  setupFiles: [
+    '<rootDir>/__mocks__/react-native.mock.js',
+    '<rootDir>/__mocks__/expo-constants.mock.js',
+    '<rootDir>/__mocks__/react-native-argon2.mock.js',
+    '<rootDir>/__mocks__/jest.setup.js',
+  ],
+  // TODO: enable this to also collect coverage
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'routes/*.ts',
+    'screens/**',
+    'machines/**',
+    'lib/jsonld-signatures/**',
+    'components/**',
+    'machines/**',
+    'shared/**',
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '^\\.\\/locales\\/en\\.json$': '<rootDir>/__mocks__/en.mock.json',
+    '^@digitalbazaar/rsa-verification-key-2018$':
+      '<rootDir>/__mocks__/rsa-verification-key-2018.js',
+    '^react-native-biometrics-changed$':
+      '<rootDir>/__mocks__/react-native-biometrics-changed.js',
+  },
 };
--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-import mock from 'jest';
-import argon2 from 'react-native-argon2';
-
-jest.mock('react-native-argon2', {
-  argon2: (pwd, salt, config) => pwd + salt,
-});
