fb5a54ae4946b5743699546be3fc3ad4d3c155a5
srikanth716 <srikanthsri7447@gmail.com>
2024-05-22 16:10:07 +0530

    refactor copilot start point
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- components/VC/Views/VCCardView.tsx
+++ components/VC/Views/VCCardView.tsx
@@ -103,7 +103,7 @@ export const VCCardView: React.FC<VCItemProps> = props => {
         onDismiss={controller.DISMISS}
         translationPath={'VcDetails'}
       />
-    </React.Fragment>
+    </View>
   );
 };
 
