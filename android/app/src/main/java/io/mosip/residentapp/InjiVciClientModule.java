022dd919be31b26d379e11b67a87b38915f290ff
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-05-23 16:39:55 +0530

    [INJIMOB-1303] modify debug log
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- android/app/src/main/java/io/mosip/residentapp/InjiVciClientModule.java
+++ android/app/src/main/java/io/mosip/residentapp/InjiVciClientModule.java
@@ -28,7 +28,7 @@ public class InjiVciClientModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void init(String appId) {
-        Log.d("InjiVciClientModule", "Initializing with " + appId);
+        Log.d("InjiVciClientModule", "Initializing InjiVciClientModule with " + appId);
         vciClient = new VCIClient(appId);
     }
 
