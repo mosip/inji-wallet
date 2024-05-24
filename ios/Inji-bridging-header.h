cdcbd973b79e0939329e1fcfc38c18d9a6255d9c
abhip2565 <74866247+abhip2565@users.noreply.github.com>
2024-05-24 23:05:41 +0530

    [INJIMOB-1267] native integration pixelpass (#1454)
    * [INJIMOB-1267] Add Native artifacts integration in Inji for Pixelpass
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB-1267] removed aar and added snapshot publication
    
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>
    
    * [INJIMOB 1267] refactor homescreen
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] refactor qroverlay component and modify native setup for pixelpass
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] refactor qroverlay component
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] changed package.resolved version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] modify native android integration setup
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] Refactor changes
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] changed native android package
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] add import for pixelpass in scanactions
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] change pixel pass version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] change pixel pass package.resolved version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    ---------
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>
    Co-authored-by: Ayushi Dey <ayushirakshitdey@gmail.com>

--- ios/Inji-bridging-header.h
+++ ios/Inji-bridging-header.h
@@ -1,5 +1,12 @@
+//
+//  Inji-bridging-header.h
+//  Inji
+//
+//  Created by Abhishek Paul on 10/05/24.
+//
+
 #ifndef Inji_bridging_header_h
 #define Inji_bridging_header_h
 #import "React/RCTBridgeModule.h"
 
-#endif
+#endif /* Inji_bridging_header_h */
