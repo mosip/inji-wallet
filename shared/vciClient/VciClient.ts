d37e7f4847399c659c34814a84fa443816cf5352
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-05-24 11:39:10 +0530

    [INJIMOB-1303] integrate vci client for android (#1453)
    * [INJIMOB-1303] add native modules for vci-client
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
    
    * [INJIMOB-1303] integrate inji-vci-client for vc download android
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
    
    * [INJIMOB-1303] change log levels for error log
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
    
    * [INJIMOB-1303] modify debug log
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
    
    * [INJIMOB-1303] rename vci client package
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- shared/vciClient/VciClient.ts
+++ shared/vciClient/VciClient.ts
@@ -1,5 +1,8 @@
 import {NativeModules} from 'react-native';
 import {__AppId} from '../GlobalVariables';
+import {isAndroid} from '../constants';
+import {request} from '../request';
+import {getCredentialRequestBody} from '../openId4VCI/Utils';
 
 export class VciClient {
   static async downloadCredential(
@@ -7,13 +10,31 @@ export class VciClient {
     jwtProof: string,
     accessToken: string,
   ) {
-    const InjiVciClient = NativeModules.InjiVciClient;
-    InjiVciClient.init(__AppId.getValue());
-    const credentialResponse = await InjiVciClient.requestCredential(
-      issuerMetaData,
-      jwtProof,
-      accessToken,
-    );
-    return JSON.parse(credentialResponse);
+    if (isAndroid()) {
+      const InjiVciClient = NativeModules.InjiVciClient;
+      InjiVciClient.init(__AppId.getValue());
+      const credentialResponse = await InjiVciClient.requestCredential(
+        issuerMetaData,
+        jwtProof,
+        accessToken,
+      );
+      return JSON.parse(credentialResponse);
+    } else {
+      const body = await getCredentialRequestBody(
+        jwtProof,
+        issuerMetaData['credentialType'],
+      );
+      return await request(
+        'POST',
+        issuerMetaData['credentialEndpoint'],
+        body,
+        '',
+        {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + accessToken,
+        },
+        issuerMetaData['downloadTimeoutInMilliSeconds'],
+      );
+    }
   }
 }
