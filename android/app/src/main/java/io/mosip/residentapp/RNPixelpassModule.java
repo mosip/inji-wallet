76b6dfd52e89220826699de08bac963438c00cbb
abhip2565 <74866247+abhip2565@users.noreply.github.com>
2024-05-24 23:05:41 +0530

    [INJIMOB-1267] native integration pixelpass (#1454)
    * [INJIMOB-1267] Add Native artifacts integration in Inji for Pixelpass
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB-1267] removed aar and added snapshot publication
    
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>
    
    * [INJIMOB 1267] refactor homescreen
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] refactor qroverlay component and modify native setup for pixelpass
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] refactor qroverlay component
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] changed package.resolved version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] modify native android integration setup
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] Refactor changes
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] changed native android package
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] add import for pixelpass in scanactions
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] change pixel pass version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB 1267] change pixel pass package.resolved version
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    ---------
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>
    Co-authored-by: Ayushi Dey <ayushirakshitdey@gmail.com>

---
+++ android/app/src/main/java/io/mosip/residentapp/RNPixelpassModule.java
@@ -0,0 +1,44 @@
+package io.mosip.residentapp;
+import io.mosip.pixelpass.PixelPass;
+import io.mosip.pixelpass.types.ECC;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import java.io.ByteArrayOutputStream;  
+import android.util.Base64;           
+import android.graphics.Bitmap;
+
+public class RNPixelpassModule extends ReactContextBaseJavaModule {
+     private PixelPass pixelPass;
+    public RNPixelpassModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        pixelPass = new PixelPass(); 
+      
+    }
+
+    @Override
+    public String getName() {
+        return "RNPixelpassModule";
+    }
+
+    @ReactMethod
+    public void decode(String parameter, Promise promise) {
+        try {
+            String text = pixelPass.decode(parameter);
+            promise.resolve(text);
+        } catch (Exception e) {
+            promise.reject("ERROR", e.toString());
+        }
+    }
+
+    @ReactMethod
+    public void generateQRData(String data, String header, Promise promise) {
+        try {
+            String qrData=pixelPass.generateQRData(data,header);
+            promise.resolve(qrData);
+        } catch (Exception e) {
+            promise.reject("ERROR_GENERATING_QR", "Failed to generate QR Data: " + e.toString());
+        }
+    }
+}
