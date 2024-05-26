1c4b290cbe7d90bc759a7be9fe49134c1829cc72
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-05-26 12:58:32 +0530

    [injimob-1313] remove platform check for the vciClient and refactor issuerMeta construction
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>

--- ios/RNVCIClientModule.swift
+++ ios/RNVCIClientModule.swift
@@ -18,35 +18,38 @@ class RNVCIClientModule: NSObject, RCTBridgeModule {
   
   @objc
   func requestCredential(_ issuerMeta: AnyObject, proof: String, accessToken: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-    Task {
-      do {
-        if let issuerMetaDict = issuerMeta as? [String: Any],
-           let credentialAudience = issuerMetaDict["credentialAudience"] as? String,
-           let credentialEndpoint = issuerMetaDict["credentialEndpoint"] as? String,
-           let downloadTimeoutInMilliseconds = issuerMetaDict["downloadTimeoutInMilliSeconds"] as? Int,
-           let credentialType = issuerMetaDict["credentialType"] as? [String],
-           let credentialFormatString = issuerMetaDict["credentialFormat"] as? String,
-           let credentialFormat = CredentialFormat(rawValue: credentialFormatString) {
-          
-          let issuerMetaObject = IssuerMeta(
-            credentialAudience: credentialAudience,
-            credentialEndpoint: credentialEndpoint,
-            downloadTimeoutInMilliseconds: downloadTimeoutInMilliseconds,
-            credentialType: credentialType,
-            credentialFormat: credentialFormat
-          )
-          
-          let response = try await vciClient!.requestCredential(issuerMeta: issuerMetaObject, proof: JWTProof(jwt: proof), accessToken: accessToken)!
-          let responseString = try response.toJSONString()
-          resolve(responseString)
-        } else {
-          reject("E_INVALID_FORMAT", "Invalid issuerMeta format", nil)
-        }
-      } catch {
-        print("Error")
-        reject("E_REQUEST_FAILED", "Request failed", error)
+      Task {
+          do {
+              guard let issuerMetaDict = issuerMeta as? [String: Any] else {
+                  reject(nil, "Invalid issuerMeta format", nil)
+                  return
+              }
+              
+              guard let credentialAudience = issuerMetaDict["credentialAudience"] as? String,
+                    let credentialEndpoint = issuerMetaDict["credentialEndpoint"] as? String,
+                    let downloadTimeoutInMilliseconds = issuerMetaDict["downloadTimeoutInMilliSeconds"] as? Int,
+                    let credentialType = issuerMetaDict["credentialType"] as? [String],
+                    let credentialFormatString = issuerMetaDict["credentialFormat"] as? String,
+                    let credentialFormat = CredentialFormat(rawValue: credentialFormatString) else {
+                  reject(nil, "Invalid issuerMeta format", nil)
+                  return
+              }
+              
+              let issuerMetaObject = IssuerMeta(
+                  credentialAudience: credentialAudience,
+                  credentialEndpoint: credentialEndpoint,
+                  downloadTimeoutInMilliseconds: downloadTimeoutInMilliseconds,
+                  credentialType: credentialType,
+                  credentialFormat: credentialFormat
+              )
+              
+              let response = try await vciClient!.requestCredential(issuerMeta: issuerMetaObject, proof: JWTProof(jwt: proof), accessToken: accessToken)!
+              let responseString = try response.toJSONString()
+              resolve(responseString)
+          } catch {
+            reject(nil,error.localizedDescription, nil)
+          }
       }
-    }
   }
   
   @objc
--- shared/vciClient/VciClient.ts
+++ shared/vciClient/VciClient.ts
@@ -1,6 +1,5 @@
 import {NativeModules} from 'react-native';
 import {__AppId} from '../GlobalVariables';
-import {isAndroid} from '../constants';
 
 export class VciClient {
   static async downloadCredential(
@@ -8,24 +7,13 @@ export class VciClient {
     jwtProof: string,
     accessToken: string,
   ) {
-    if (isAndroid()) {
-      const InjiVciClient = NativeModules.InjiVciClient;
-      InjiVciClient.init(__AppId.getValue());
-      const credentialResponse = await InjiVciClient.requestCredential(
-        issuerMetaData,
-        jwtProof,
-        accessToken,
-      );
-      return JSON.parse(credentialResponse);
-    } else {
-      const {InjiVciClient} = NativeModules;
-      InjiVciClient.init(__AppId.getValue());
-      const credentialResponse = await InjiVciClient.requestCredential(
-        issuerMetaData,
-        jwtProof,
-        accessToken,
-      );
-      return JSON.parse(credentialResponse);
-    }
+    const InjiVciClient = NativeModules.InjiVciClient;
+    InjiVciClient.init(__AppId.getValue());
+    const credentialResponse = await InjiVciClient.requestCredential(
+      issuerMetaData,
+      jwtProof,
+      accessToken,
+    );
+    return JSON.parse(credentialResponse);
   }
 }
