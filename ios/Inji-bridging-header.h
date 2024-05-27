a560177ab7afe3b11d064d39b8a3dab19ebe1e77
PoojaBabusing <115976560+PoojaBabusing@users.noreply.github.com>
2024-05-27 09:52:30 +0530

    [injimob-1313] add native module briding files for inji vci client ios (#1459)
    * [injimob-1313] add native module briding files for inji vci client ios
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client version
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client version
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    Co-authored-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>

--- ios/Inji-bridging-header.h
+++ ios/Inji-bridging-header.h
@@ -1,12 +1,5 @@
-//
-//  Inji-bridging-header.h
-//  Inji
-//
-//  Created by Abhishek Paul on 10/05/24.
-//
-
 #ifndef Inji_bridging_header_h
 #define Inji_bridging_header_h
 #import "React/RCTBridgeModule.h"
 
-#endif /* Inji_bridging_header_h */
+#endif
