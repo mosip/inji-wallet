33a70c5034f00e7da125675e66269c35f90615ff
PoojaBabusing <115976560+PoojaBabusing@users.noreply.github.com>
2024-05-27 09:52:30 +0530

    [injimob-1313] add native module briding files for inji vci client ios (#1459)
    * [injimob-1313] add native module briding files for inji vci client ios
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client version
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    * [injimob-1313] update vci client version
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    Co-authored-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>

--- shared/vciClient/VciClient.ts
+++ shared/vciClient/VciClient.ts
@@ -1,8 +1,5 @@
 import {NativeModules} from 'react-native';
 import {__AppId} from '../GlobalVariables';
-import {isAndroid} from '../constants';
-import {request} from '../request';
-import {getCredentialRequestBody} from '../openId4VCI/Utils';
 
 export class VciClient {
   static async downloadCredential(
@@ -10,31 +7,13 @@ export class VciClient {
     jwtProof: string,
     accessToken: string,
   ) {
-    if (isAndroid()) {
-      const InjiVciClient = NativeModules.InjiVciClient;
-      InjiVciClient.init(__AppId.getValue());
-      const credentialResponse = await InjiVciClient.requestCredential(
-        issuerMetaData,
-        jwtProof,
-        accessToken,
-      );
-      return JSON.parse(credentialResponse);
-    } else {
-      const body = await getCredentialRequestBody(
-        jwtProof,
-        issuerMetaData['credentialType'],
-      );
-      return await request(
-        'POST',
-        issuerMetaData['credentialEndpoint'],
-        body,
-        '',
-        {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + accessToken,
-        },
-        issuerMetaData['downloadTimeoutInMilliSeconds'],
-      );
-    }
+    const InjiVciClient = NativeModules.InjiVciClient;
+    InjiVciClient.init(__AppId.getValue());
+    const credentialResponse = await InjiVciClient.requestCredential(
+      issuerMetaData,
+      jwtProof,
+      accessToken,
+    );
+    return JSON.parse(credentialResponse);
   }
 }
