f240ac1d2d69996fa6cfe27b416f8e8686a92d97
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-06-03 23:19:31 +0530

    [INJIMOB-1192] move vc item machine events into model
    Events should not be exported to other packages for direct use so that Xstate's createModel() can decorate the function appropriately
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- machines/VerifiableCredential/VCItemMachine/VCItemModel.ts
+++ machines/VerifiableCredential/VCItemMachine/VCItemModel.ts
@@ -4,12 +4,44 @@ import {VCMetadata} from '../../../shared/VCMetadata';
 import {
   Credential,
   DecodedCredential,
-  VerifiableCredential,
+  VC,
   WalletBindingResponse,
 } from '../VCMetaMachine/vc';
 import {CommunicationDetails} from '../../../shared/Utils';
-import {VCItemEvents} from './VCItemEvents';
 import {vcVerificationBannerDetails} from '../../../components/BannerNotificationContainer';
+
+const VCItemEvents = {
+  DISMISS: () => ({}),
+  CREDENTIAL_DOWNLOADED: (response: VC) => ({response}),
+  STORE_RESPONSE: (response: VC) => ({response}),
+  STORE_ERROR: (error: Error) => ({error}),
+  POLL: () => ({}),
+  DOWNLOAD_READY: () => ({}),
+  FAILED: () => ({}),
+  GET_VC_RESPONSE: (response: VC) => ({response}),
+  INPUT_OTP: (OTP: string) => ({OTP}),
+  RESEND_OTP: () => ({}),
+  REFRESH: () => ({}),
+  ADD_WALLET_BINDING_ID: () => ({}),
+  CANCEL: () => ({}),
+  CONFIRM: () => ({}),
+  PIN_CARD: () => ({}),
+  KEBAB_POPUP: () => ({}),
+  SHOW_ACTIVITY: () => ({}),
+  CLOSE_VC_MODAL: () => ({}),
+  REMOVE: (vcMetadata: VCMetadata) => ({vcMetadata}),
+  UPDATE_VC_METADATA: (vcMetadata: VCMetadata) => ({vcMetadata}),
+  TAMPERED_VC: (key: string) => ({key}),
+  SHOW_BINDING_STATUS: () => ({}),
+  VERIFY: () => ({}),
+  SET_VERIFICATION_STATUS: (response: unknown) => ({response}),
+  RESET_VERIFICATION_STATUS: () => ({}),
+  REMOVE_VERIFICATION_STATUS_BANNER: () => ({}),
+  SHOW_VERIFICATION_STATUS_BANNER: (response: unknown) => ({response}),
+  UPDATE_WELLKNOWN_RESPONSE: (wellknownResponse: Object) => ({
+    wellknownResponse,
+  }),
+};
 
 export const VCItemModel = createModel(
   {
