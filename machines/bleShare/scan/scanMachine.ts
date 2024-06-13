b800d5214f332f118e9dbd9ff6c9989c824cffd9
Srikanth Kola <97477121+srikanth716@users.noreply.github.com>
2024-06-13 10:23:23 +0530

    [INJIMOB-1242]: fix face verification banner toaster for normal sharing (#1489)
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- machines/bleShare/scan/scanMachine.ts
+++ machines/bleShare/scan/scanMachine.ts
@@ -306,6 +306,7 @@ export const scanMachine =
             'registerLoggers',
             'clearUri',
             'setChildRef',
+            'resetFaceCaptureBannerStatus',
           ],
           on: {
             SCAN: [
