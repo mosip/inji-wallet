c83dd77b17711e53993bc6a19c093dc234a8e8b4
Alka Prasad <prasadalka1998@gmail.com>
2024-07-10 03:34:24 +0530

    [INJIMOB-891]: use function available in constant for platform check
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- shared/tuvali/tuvali.ts
+++ shared/tuvali/tuvali.ts
@@ -1,14 +1,15 @@
 import {NativeEventEmitter, NativeModules, Platform} from 'react-native';
 import type {Verifier, Wallet} from './types/interface';
+import {isIOS} from '../constants';
 
 const LINKING_ERROR =
-  `The package '@mosip/tuvali' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'tuvali' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
 const VERIFIER_NOT_IMPLEMENTATED_ERROR = `Verifier is not yet implemented on IOS. Please remove Verifier usage on IOS Platform`;
-const isIOS = Platform.OS === 'ios';
+const isIOSPlatform = isIOS();
 
 export const wallet: Wallet = NativeModules.WalletModule
   ? NativeModules.WalletModule
@@ -28,13 +29,13 @@ export let verifier: Verifier = NativeModules.VerifierModule
       {
         get() {
           throw new Error(
-            isIOS ? VERIFIER_NOT_IMPLEMENTATED_ERROR : LINKING_ERROR,
+            isIOSPlatform ? VERIFIER_NOT_IMPLEMENTATED_ERROR : LINKING_ERROR,
           );
         },
       },
     );
 
-if (!isIOS) {
+if (!isIOSPlatform) {
   setupModule(verifier);
 }
 
@@ -47,7 +48,7 @@ function setupModule(module: any) {
       eventEmitter.addListener('DATA_EVENT', callback);
   }
 
-  if (isIOS) {
+  if (isIOSPlatform) {
     const eventEmitter = new NativeEventEmitter(module);
     module.handleDataEvents = (callback: (event: any) => void) =>
       eventEmitter.addListener('DATA_EVENT', callback);
