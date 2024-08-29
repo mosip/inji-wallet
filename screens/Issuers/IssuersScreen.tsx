4d3e2c212173804814380c2bbb7f19793bf8e662
Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
2024-08-29 13:04:13 +0530

    [INJIMOB-1854] modify error messages for wellknown api call failure
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- screens/Issuers/IssuersScreen.tsx
+++ screens/Issuers/IssuersScreen.tsx
@@ -1,16 +1,16 @@
-import React, {useLayoutEffect, useState} from 'react';
-import {useTranslation} from 'react-i18next';
-import {FlatList, Pressable, View} from 'react-native';
-import {Issuer} from '../../components/openId4VCI/Issuer';
-import {Error} from '../../components/ui/Error';
-import {Header} from '../../components/ui/Header';
-import {Button, Column, Row, Text} from '../../components/ui';
-import {Theme} from '../../components/ui/styleUtils';
-import {RootRouteProps} from '../../routes';
-import {HomeRouteProps} from '../../routes/routeTypes';
-import {useIssuerScreenController} from './IssuerScreenController';
-import {Loader} from '../../components/ui/Loader';
-import {removeWhiteSpace} from '../../shared/commonUtil';
+import React, { useLayoutEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FlatList, Pressable, View } from 'react-native';
+import { Issuer } from '../../components/openId4VCI/Issuer';
+import { Error } from '../../components/ui/Error';
+import { Header } from '../../components/ui/Header';
+import { Button, Column, Row, Text } from '../../components/ui';
+import { Theme } from '../../components/ui/styleUtils';
+import { RootRouteProps } from '../../routes';
+import { HomeRouteProps } from '../../routes/routeTypes';
+import { useIssuerScreenController } from './IssuerScreenController';
+import { Loader } from '../../components/ui/Loader';
+import { removeWhiteSpace } from '../../shared/commonUtil';
 import {
   ErrorMessage,
   getDisplayObjectForCurrentLanguage,
@@ -22,19 +22,19 @@ import {
   sendInteractEvent,
   sendStartEvent,
 } from '../../shared/telemetry/TelemetryUtils';
-import {TelemetryConstants} from '../../shared/telemetry/TelemetryConstants';
-import {MessageOverlay} from '../../components/MessageOverlay';
-import {SearchBar} from '../../components/ui/SearchBar';
-import {SvgImage} from '../../components/ui/svg';
-import {Icon} from 'react-native-elements';
-import {BannerNotificationContainer} from '../../components/BannerNotificationContainer';
-import {CredentialTypeSelectionScreen} from './CredentialTypeSelectionScreen';
+import { TelemetryConstants } from '../../shared/telemetry/TelemetryConstants';
+import { MessageOverlay } from '../../components/MessageOverlay';
+import { SearchBar } from '../../components/ui/SearchBar';
+import { SvgImage } from '../../components/ui/svg';
+import { Icon } from 'react-native-elements';
+import { BannerNotificationContainer } from '../../components/BannerNotificationContainer';
+import { CredentialTypeSelectionScreen } from './CredentialTypeSelectionScreen';
 
 export const IssuersScreen: React.FC<
   HomeRouteProps | RootRouteProps
 > = props => {
   const controller = useIssuerScreenController(props);
-  const {t} = useTranslation('IssuersScreen');
+  const { t } = useTranslation('IssuersScreen');
 
   const issuers = controller.issuers;
   let [filteredSearchData, setFilteredSearchData] = useState(issuers);
@@ -77,7 +77,7 @@ export const IssuersScreen: React.FC<
 
   const onPressHandler = (id: string, protocol: string) => {
     sendStartEvent(
-      getStartEventData(TelemetryConstants.FlowType.vcDownload, {id: id}),
+      getStartEventData(TelemetryConstants.FlowType.vcDownload, { id: id }),
     );
     sendInteractEvent(
       getInteractEventData(
@@ -124,9 +124,11 @@ export const IssuersScreen: React.FC<
   };
 
   const getImage = () => {
-    if (isGenericError() || isBackendError()) {
+    if (isGenericError()) {
       return SvgImage.SomethingWentWrong();
     }
+    if (isBackendError())
+      return SvgImage.ErrorOccurred()
     return SvgImage.NoInternetConnection();
   };
 
@@ -166,7 +168,7 @@ export const IssuersScreen: React.FC<
         primaryButtonText="goBack"
         primaryButtonEvent={controller.RESET_VERIFY_ERROR}
         primaryButtonTestID="goBack"
-        customStyles={{marginTop: '30%'}}
+        customStyles={{ marginTop: '30%' }}
       />
     );
   }
@@ -210,7 +212,7 @@ export const IssuersScreen: React.FC<
         image={getImage()}
         showClose
         primaryButtonTestID="tryAgain"
-        primaryButtonText="tryAgain"
+        primaryButtonText={controller.errorMessageType!=ErrorMessage.TECHNICAL_DIFFICULTIES?"tryAgain":undefined}
         primaryButtonEvent={controller.TRY_AGAIN}
         onDismiss={goBack}
       />
@@ -270,7 +272,7 @@ export const IssuersScreen: React.FC<
             {controller.issuers.length > 0 && (
               <FlatList
                 data={filteredSearchData}
-                renderItem={({item}) => (
+                renderItem={({ item }) => (
                   <Issuer
                     testID={removeWhiteSpace(item.credential_issuer)}
                     key={item.credential_issuer}
