59b664baa0991648996c77c8d2fe57f46411b4a9
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-09-19 20:08:28 +0530

    [INJIMOB-1629] make changes in card skeleton component to show vc card loader in vp sharing flow
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- components/VC/common/VCCardSkeleton.tsx
+++ components/VC/common/VCCardSkeleton.tsx
@@ -4,9 +4,52 @@ import {ImageBackground, View} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import ShimmerPlaceholder from 'react-native-shimmer-placeholder';
+import {VCItemContainerFlowType, VCShareFlowType} from '../../../shared/Utils';
 
-export const VCCardSkeleton = () => {
-  return (
+export const VCCardSkeleton: React.FC<VCCardSkeletonProps> = props => {
+  return props.flow === VCItemContainerFlowType.OPENID4VP ? (
+    <View style={Theme.Styles.closeCardBgContainer}>
+      <ImageBackground
+        source={Theme.CloseCard}
+        resizeMode="stretch"
+        style={Theme.Styles.vertloadingContainer}>
+        <Column>
+          <Row crossAlign="center">
+            <ShimmerPlaceholder
+              LinearGradient={LinearGradient}
+              width={22}
+              height={22}
+              style={{borderRadius: 5, marginRight: 20, marginLeft: 15}}
+            />
+            <ShimmerPlaceholder
+              LinearGradient={LinearGradient}
+              width={40}
+              height={53}
+              style={{borderRadius: 5}}
+            />
+            <Column fill align={'space-around'} margin={'0 10 0 10'}>
+              <ShimmerPlaceholder
+                LinearGradient={LinearGradient}
+                width={100}
+                style={{borderRadius: 5, marginTop: 5}}
+              />
+              <ShimmerPlaceholder
+                LinearGradient={LinearGradient}
+                width={50}
+                style={{borderRadius: 5, marginTop: 5}}
+              />
+            </Column>
+            <ShimmerPlaceholder
+              LinearGradient={LinearGradient}
+              width={35}
+              height={35}
+              style={{borderRadius: 5}}
+            />
+          </Row>
+        </Column>
+      </ImageBackground>
+    </View>
+  ) : (
     <View style={Theme.Styles.closeCardBgContainer}>
       <ImageBackground
         source={Theme.CloseCard}
@@ -50,3 +93,7 @@ export const VCCardSkeleton = () => {
     </View>
   );
 };
+
+export interface VCCardSkeletonProps {
+  flow?: string;
+}
