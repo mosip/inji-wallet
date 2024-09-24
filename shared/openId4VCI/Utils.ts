1d7e12c824c0ceca020f0f16dc7825aeaea08b89
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-09-24 20:37:59 +0530

    [INJIMOB-2005] revert harcode token endpoint to token fetching issue (#1625)
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- shared/openId4VCI/Utils.ts
+++ shared/openId4VCI/Utils.ts
@@ -137,9 +137,7 @@ export const constructAuthorizationConfiguration = (
     serviceConfiguration: {
       authorizationEndpoint:
         selectedIssuer.authorization_servers[0] + '/authorize',
-      tokenEndpoint:
-        'https://demos-injiweb.collab.mosip.net/v1/mimoto/get-token/' +
-        selectedIssuer.credential_issuer,
+      tokenEndpoint: selectedIssuer.token_endpoint,
     },
   };
 };
