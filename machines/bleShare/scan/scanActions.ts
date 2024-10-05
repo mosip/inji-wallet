92ca20d00b9422b40dcecb83f034b42a5389dfcd
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-10-05 21:56:28 +0530

    [INJIMOB-812] remove unused import in scan actions
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- machines/bleShare/scan/scanActions.ts
+++ machines/bleShare/scan/scanActions.ts
@@ -29,10 +29,7 @@ import {StoreEvents} from '../../store';
 import BluetoothStateManager from 'react-native-bluetooth-state-manager';
 import {NativeModules} from 'react-native';
 import {wallet} from '../../../shared/tuvali';
-import {
-  createOpenID4VPMachine,
-  OpenID4VPEvents,
-} from '../../openID4VP/openID4VPMachine';
+import {createOpenID4VPMachine} from '../../openID4VP/openID4VPMachine';
 
 const QR_LOGIN_REF_ID = 'QrLogin';
 const OPENID4VP_REF_ID = 'OpenID4VP';
