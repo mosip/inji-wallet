e572faf30b9dd8228d6f6970ed5476e47d69d53f
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-12-13 11:30:44 +0530

    [INJIMOB-2538] fetch the openid4vp sharing flow client validation property from inji default properties file and pass it to library (#1739)
    * [INJIMOB-2538] fetch whether we should do client validation or not in VP sharing flow based on the value assigned in inji config default properties file
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    
    * [INJIMOB-2538] convert clientValidation property from string to boolean before passing it to library
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    
    * [INJIMOB-2538] change the logic of checking the value of clientValidation property
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    
    * [INJIMOB-2538] rename clientValidation variable to shouldValiateClient and call verifiers api only if validation is required
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    
    * [INJIMOB-2538] check if client validation is needed or not as part of service instead of action
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    
    * [INJIMOB-2538] update Package.resolved
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    ---------
    
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    Co-authored-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- ios/RNOpenID4VPModule.m
+++ ios/RNOpenID4VPModule.m
@@ -7,6 +7,7 @@ RCT_EXTERN_METHOD(init:(NSString *)appId)
 
 RCT_EXTERN_METHOD(authenticateVerifier:(NSString *)encodedAuthorizationRequest
                   trustedVerifierJSON:(id)trustedVerifierJSON
+                  shouldValidateClient:(BOOL)shouldValidateClient
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 
