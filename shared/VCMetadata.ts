1440fefd44a85b84d865eb0005e86478134c7907
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2024-12-16 18:55:43 +0530

    [INJIMOB-2471] maintain consistency in setting VCMetadata ID
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- shared/VCMetadata.ts
+++ shared/VCMetadata.ts
@@ -123,17 +123,19 @@ export const getVCMetadata = (context: object, keyType: string, credType: Creden
   const [issuer, protocol, credentialId] =
     context.credentialWrapper?.identifier.split(':');
 
+  //TODO: Can we get the issuer and protocol as context.selectedIssuer.credential_issuer  and context.selectedIssuer.protocol respectively?
+  // This will avoid setting identifier field in credential wrapper and splitting it to get the details
   return VCMetadata.fromVC({
     requestId: credentialId ?? null,
     issuer: issuer,
     protocol: protocol,
-    id: `${credentialId} + '_' + ${issuer}`,
+    id: `${credentialId}_${issuer}`,
     timestamp: context.timestamp ?? '',
     isVerified: context.vcMetadata.isVerified ?? false,
     isExpired: context.vcMetadata.isExpired ?? false,
     mosipIndividualId: getMosipIndividualId(
       context['verifiableCredential'] as VerifiableCredential,
-      issuer
+      issuer,
     ),
     format: context['credentialWrapper'].format,
     downloadKeyType: keyType,
@@ -143,7 +145,7 @@ export const getVCMetadata = (context: object, keyType: string, credType: Creden
 
 const getMosipIndividualId = (
   verifiableCredential: VerifiableCredential | Credential,
-  issuer: string
+  issuer: string,
 ) => {
   try {
     const credential = verifiableCredential?.credential
