033b4d7c0c520a49d5f7d902c6f1282671976fb3
abhip2565 <paul.apaul.abhishek.AP@gmail.com>
2025-01-31 12:07:34 +0530

    [INJIMOB-2823] remove display of keyType in VC detail view (#1770)
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- components/VC/common/VCUtils.tsx
+++ components/VC/common/VCUtils.tsx
@@ -45,7 +45,6 @@ export const DETAIL_VIEW_BOTTOM_SECTION_FIELDS = [
   'credentialRegistry',
 ];
 
-export const KEY_TYPE_FIELD = 'keytype';
 export const BOTTOM_SECTION_FIELDS_WITH_DETAILED_ADDRESS_FIELDS = [
   ...getAddressFields(),
   'email',
