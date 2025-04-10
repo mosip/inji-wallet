affeb5cff8c1229b2e5d41aee75531fe26aa2d69
Alka Prasad <prasadalka1998@gmail.com>
2025-04-10 13:41:17 +0530

    [INJIMOB-2799]: add support for sending wallet metadata to verifier (#1875)
    * [INJIMOB-2799]: add support for sending wallet metadata to verifier
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-3157]: [iOS]add support to send wallet metadata to verifier
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: [iOS] pass client id schemes only if it is present and not empty
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-2491]: should read walletMetadata from config if present else from the local
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-3157]:[iOS]update validations on wallet metadata
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: update openid4vp swift package
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: fix native module issue
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: refactor wallet metadata to pass optional values
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: refactor wallet metadata to pass optional values
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    * [INJIMOB-3157]: update openid4vp swift package
    
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    
    ---------
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>
    Co-authored-by: adityankannan-tw <adityan.kannan@thoughtworks.com>

--- ios/RNOpenID4VPModule.swift
+++ ios/RNOpenID4VPModule.swift
@@ -19,6 +19,7 @@ class RNOpenId4VpModule: NSObject, RCTBridgeModule {
   @objc
   func authenticateVerifier(_ urlEncodedAuthorizationRequest: String,
                             trustedVerifierJSON: AnyObject,
+                            walletMetadata: AnyObject?,
                             shouldValidateClient: Bool,
                             resolver resolve: @escaping RCTPromiseResolveBlock,
                             rejecter reject: @escaping RCTPromiseRejectBlock) {
@@ -37,7 +38,14 @@ class RNOpenId4VpModule: NSObject, RCTBridgeModule {
           return Verifier(clientId: clientId, responseUris: responseUris)
         }
         
-        let authenticationResponse: AuthorizationRequest = try await openID4VP!.authenticateVerifier(urlEncodedAuthorizationRequest: urlEncodedAuthorizationRequest, trustedVerifierJSON: trustedVerifiersList, shouldValidateClient: shouldValidateClient)
+        let walletMetadataObject = try getWalletMetadataFromDict(walletMetadata as Any, reject: reject)
+        
+        let authenticationResponse: AuthorizationRequest = try await openID4VP!.authenticateVerifier(
+          urlEncodedAuthorizationRequest: urlEncodedAuthorizationRequest,
+          trustedVerifierJSON: trustedVerifiersList,
+          walletMetadata: walletMetadataObject,
+          shouldValidateClient: shouldValidateClient
+        )
         
         let response = try toJsonString(jsonObject: authenticationResponse)
         resolve(response)
@@ -174,3 +182,37 @@ extension Dictionary {
     Dictionary<T, Value>(uniqueKeysWithValues: map { (transform($0.key), $0.value) })
   }
 }
+
+func getWalletMetadataFromDict(
+  _ walletMetadata: Any,
+  reject: RCTPromiseRejectBlock
+) throws -> WalletMetadata {
+  
+  guard let metadata = walletMetadata as? [String: Any] else {
+    reject("OPENID4VP", "Invalid wallet metadata format", nil)
+    throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid Wallet Metadata data"])
+  }
+  
+  var vpFormatsSupported: [String: VPFormatSupported] = [:]
+  if
+    let vpFormatsSupportedDict = metadata["vp_formats_supported"] as? [String: Any],
+    let ldpVcDict = vpFormatsSupportedDict["ldp_vc"] as? [String: Any]
+  {
+    let algValuesSupported = ldpVcDict["alg_values_supported"] as? [String]
+    vpFormatsSupported["ldp_vc"] = VPFormatSupported(algValuesSupported: algValuesSupported)
+  } else {
+    vpFormatsSupported["ldp_vc"] = VPFormatSupported(algValuesSupported: nil)
+  }
+  
+  let walletMetadataObject = try WalletMetadata(
+    presentationDefinitionURISupported: metadata["presentation_definition_uri_supported"] as? Bool,
+    vpFormatsSupported: vpFormatsSupported,
+    clientIdSchemesSupported: metadata["client_id_schemes_supported"] as? [String],
+    requestObjectSigningAlgValuesSupported: metadata["request_object_signing_alg_values_supported"] as? [String],
+    authorizationEncryptionAlgValuesSupported: metadata["authorization_encryption_alg_values_supported"] as? [String],
+    authorizationEncryptionEncValuesSupported: metadata["authorization_encryption_enc_values_supported"] as? [String]
+  )
+  
+  return walletMetadataObject
+}
+
