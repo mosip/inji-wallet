197625c39f4601e81a468497937b2678ef6bf32e
adityankannan-tw <adityan.kannan@thoughtworks.com>
2025-04-11 18:38:41 +0530

    [INJIMOB-3181]: add deep link support for ios
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>

---
+++ ios/IntentData.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+@objc public class IntentData: NSObject {
+    @objc public static let shared = IntentData()
+    private let syncQueue = DispatchQueue(label: "com.intentdata.syncQueue", attributes: .concurrent)
+    private var _qrData: String = ""
+
+    private override init() {
+        super.init()
+    }
+
+    @objc public func getQrData() -> String {
+        var data: String = ""
+        syncQueue.sync {
+            data = _qrData
+        }
+        return data
+    }
+
+    @objc public func updateQrData(_ newValue: String) {
+        syncQueue.async(flags: .barrier) {
+            self._qrData = newValue
+        }
+    }
+
+    @objc public var qrData: String {
+        get {
+            return getQrData()
+        }
+        set {
+            updateQrData(newValue)
+        }
+    }
+}
